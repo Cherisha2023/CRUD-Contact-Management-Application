570ea520b3bdbbfdbadf60694c2e87be7170a6ac
Cherisha <cherishasings@gmail.com>
2025-07-19 15:22:24 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 import app from './app.js';
 import http from 'http';
 
-const PORT = 5050;
+// Use dynamic port or fallback to 5050
+const PORT = process.env.PORT || 5050;
 
 const server = http.createServer(app);
 
 server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`✅ Server running on http://localhost:${PORT}`);
 });
