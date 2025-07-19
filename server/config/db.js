4df3ffb69254c2e67ea67996810475ec22f71675
Cherisha <cherishasings@gmail.com>
2025-07-19 15:42:06 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,17 @@
 import pkg from 'pg';
 const { Pool } = pkg;
+import dotenv from 'dotenv';
+dotenv.config();
 
 const pool = new Pool({
-  user: 'cherisha.s',
-  host: 'localhost',
-  database: 'contact_app',
-  password: 'Cheri@123',
-  port: 5432, // or your DB port
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false, // required for external hosted DBs like Render, Supabase, etc.
+  },
 });
 
 export default pool;
